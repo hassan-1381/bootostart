6587c34dfe96726d245615c79a9a7342a430f6db
hassan agabalazada <hassanagabalazada81tr@gmail.com>
2023-02-25 12:34:51 +0330

    beginning of the project

---
+++ src/bootstrap.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+ export default  class Bootstrap extends React.Component {
+
+
+    render() {
+        return (
+            <>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+            </>
+        )
+    }
+}
+

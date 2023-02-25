6fb2d15c52e3131c528c385414796a6b0e10942b
hassan agabalazada <hassanagabalazada81tr@gmail.com>
2023-02-25 23:19:09 +0330

    startMenu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React ,{Component} from "react";
+import Menu from "./script/Menu";
+import Mobilmenu from "./script/Mobilmenu";
+
+import "./style/style.css";
 
 
+export default class App extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            logo:"./public/img/logo.webp",
+        }
+    }
+    render() {
+        return (
+            <>
+                <div className="container">
+                    <Menu />
+                    <Mobilmenu/>
+          
+                </div>
 
-function App() {
-  return (
-    <div className="App">
+
+            </>
+        )
+
+    }
 
 
-    </div>
-  );
 }
-
-export default App;
